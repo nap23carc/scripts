6e7f7a51f82e6899eab37607f64ce7338c795d56
Nicholas Porter <132295364+nap23carc@users.noreply.github.com>
2023-05-10 08:46:47 -0600

    Update serverstats.sh

--- serverstats.sh
+++ serverstats.sh
@@ -2,38 +2,26 @@
 ##################################################
 #                                                #
 #              Server Status Report              #
-#                  v0.1 - 2023                   #
-#                                                # 
-#          Nick Porter / Anneliese Ward          #
+#                  v0.2 - 2023                   #
+#                                                #
+#        Nicholas Porter / Anneliese Ward        #
 #         nap23@unm.edu / anw0412@unm.edu        #
 #        Research Computing Facilitators         #
 #     Center for Advanced Research Computing     #
 #            University of New Mexico            #
-#                                                # 
-#      https://tinyurl.com/carcserverreport      #
 #                                                #
 ##################################################
-
-echo What is the name of this server?
-
+echo "What is the server's name?"
 read servername
-
 d=$(date +%Y-%m-%d-%H:%M:%S)
-
 {
-	echo -e "-----------------Operating System-----------------\n"
-	hostnamectl
-	echo -e "\n-----------------Memory Status-----------------\n"
-	free -h
-	echo -e "\n-----------------Mounted Devices-----------------\n"
-	lsblk
-	echo -e "\n-----------------Network Status-----------------\n"
-	nmcli
-	echo -e "\n-----------------Listening UDP/TCP Ports-----------------\n"
-	ss -ltu
-	echo -e "\n-----------------Systemd Status-----------------\n"
-	systemctl status
-	echo -e "\n-----------------Failed Services-----------------\n"
-	systemctl --failed
-	echo -e "\n-----------------End of Report-----------------"
+	echo -e "##### BEGINNING OF REPORT #####"
+	echo -e "\n----- Operating System -----\n"; hostnamectl
+	echo -e "\n----- Memory Status -----\n"; free -h
+	echo -e "\n----- Mounted Devices -----\n"; lsblk
+	echo -e "\n----- Network Status -----\n"; nmcli
+	echo -e "\n----- Listening UDP/TCP Ports -----\n"; ss -ltu
+	echo -e "\n----- Running Services -----\n"; systemctl --type=service --state=running
+	echo -e "\n----- Failed Services -----\n"; systemctl --failed
+	echo -e "\n##### END OF REPORT #####"
 } > $servername-status-report-$d.txt
