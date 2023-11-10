c7210c8a5eb368194bd777e4db74487be9a24cca
Nicholas Porter <132295364+nap23carc@users.noreply.github.com>
2023-11-10 14:47:08 -0700

    Update serverstats.sh

--- serverstats.sh
+++ serverstats.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ##################################################
 #              Server Status Report              #
-#                  v0.3 - 2023                   #
+#                  v0.5 - 2023                   #
 #        Nicholas Porter / Anneliese Ward        #
 #         nap23@unm.edu / anw0412@unm.edu        #
 #        Research Computing Facilitators         #
@@ -9,15 +9,18 @@
 #            University of New Mexico            #
 ##################################################
 server=$(hostname)
-date=$(date +%Y-%m-%d_%H-%M-%S)
+date=$(date +%Y-%m-%d_%H-%M)
 {
   echo -e "##### BEGINNING OF REPORT #####"
   echo -e "\n----- Operating System -----\n"; hostnamectl
   echo -e "\n----- Memory Status -----\n"; free -h
-  echo -e "\n----- Mounted Devices -----\n"; lsblk
-  echo -e "\n----- Network Status -----\n"; nmcli
+  echo -e "\n----- Mounted Devices -----\n"; lsblk -f
+  echo -e "\n----- Mounted Filesystems -----\n"; findmnt
+  echo -e "\n----- Network Device Status -----\n"; nmcli device show
   echo -e "\n----- Listening UDP/TCP Ports -----\n"; ss -ltu
   echo -e "\n----- Running Services -----\n"; systemctl --type=service --state=running
   echo -e "\n----- Failed Services -----\n"; systemctl --failed
+  echo -e "\n----- Users -----\n"; getent passwd
+  echo -e "\n----- Groups -----\n"; getent group
   echo -e "\n##### END OF REPORT #####"
-} > $server-status-report-$date.txt 2>&1
+} > $server-report-$date.txt 2>&1
